57052b107ef7daa9095fffef185d478b62bd8231
EdCh-Lo <e.louis@majelan.audio>
2022-06-15 11:53:56 +0200

    clean sql file

--- api/data/create_db.sql
+++ api/data/create_db.sql
@@ -1,34 +1,13 @@
-/* 
-    En tant qu'utilisateur postgres qui a tous les droits (cf fiche recap)
-    On a crée au préallable un user avec
-    CREATE USER truc WITH PASSWORD 'much';
-    qui est l'équivalent de CREATE ROLE truc WITH LOGIN PASSWORD 'much';
-    si on s'est trompé on peut modifier avec ALTER
-    ALTER ROLE truc WITH PASSWORD 'machin';
 
-    On a ensuite créer la database avec un propriétaire
-    CREATE DATABASE bidule WITH OWNER truc;
-
-    Le owner a tous les droits sur la database on aurait pu aussi gérer des droits plus précis avec GRANT https://www.postgresql.org/docs/9.0/sql-grant.html
-*/
-
-BEGIN; -- BEGIN va de paire avec COMMIT ici en fin de fichier
--- ainsi si on a des soucis dans les instructions qui suivent, rien ne sera fait
--- si pas de soucis tout sera fait, ça évite de faire des choses à moitié si on a des bugs
--- on dit qu'on fait une transaction
+BEGIN; 
 
 DROP TABLE IF EXISTS "list", "card", "label", "card_has_label";
--- on efface et on recommence, ainsi si on pourri les tables dans nos tests
--- on aura juste à relancer le script de création
 
 CREATE TABLE "list" (
     "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    -- la contrante GENERATED AS IDENTITY sur INTEGER est un équivalent standard de SERIAL
-    -- avec ALWAYS les id seront générés automatiquement tout le temps, pas besoin de s'en soucier
     "title" TEXT NOT NULL,
     "position" SMALLINT NOT NULL DEFAULT 0,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    -- la colonne created_at a une valeur par défaut donc en fait ça mange pas de pain, ça va se remplir tout seul
     "updated_at" TIMESTAMPTZ
 );
 
@@ -40,7 +19,6 @@ CREATE TABLE "card" (
   "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   "updated_at" TIMESTAMPTZ,
   "list_id" INTEGER NOT NULL REFERENCES "list"("id") ON DELETE CASCADE
-  -- la contrainte ON DELETE CASCADE permet de dire si on supprime une liste, on supprime les cartes qui y font référence
 );
 
 CREATE TABLE "label" (
@@ -56,11 +34,9 @@ CREATE TABLE "card_has_label" (
   "label_id" INTEGER NOT NULL REFERENCES "label"("id") ON DELETE CASCADE,
   "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   PRIMARY KEY ("card_id", "label_id")
-  -- on crée une clé composition, cad la combinaison de plusieurs champs
-  -- ainsi ici chaque combinaison de card_id/label_id devra etre unique
 );
 
-INSERT INTO "list" ("title", "position") -- on peut lister uniquement les champs qui nous intéresse
+INSERT INTO "list" ("title", "position")
     VALUES 
         ('To Buy', 0),
         ('To Do', 1); 
